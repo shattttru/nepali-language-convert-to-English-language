38318d95f3e8fdd96a9b1fac44a191ce4140da08
Shatrudhan Prasad Sharma <119649847+shattttru@users.noreply.github.com>
2024-04-05 11:08:00 +0530

    Add files via upload
    Nepali Language Convert to English Language 
    share my website and follow me

---
+++ countries.js
@@ -0,0 +1,21 @@
+const countries = {
+    "am-ET": "Amharic",
+    "ar-SA": "Arabic",
+    "be-BY": "Bielarus",
+    "bem-ZM": "Bemba",
+    "bi-VU": "Bislama",
+    "bn-IN": "Bengali",
+    "en-GB": "English",
+    "de-DE": "German",
+    "es-ES": "Spanish",
+    "el-GR": "Greek",
+    "ta-LK": "Tamil",
+    "te-IN": "Telugu",
+    "hi-IN": "Hindi", 
+    "ja-jp": "Japanese",
+    "gu-IN": "Gujarati",
+    "ko-KR": "Korean",
+    "fr-FR": "French",
+    "ne-NP": "Nepali",
+    "ur-PK": "Pakistani"
+}
---
+++ script.js
@@ -0,0 +1,28 @@
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+  selectTag = document.querySelectorAll("select"),
+  translateBtn = document.querySelector("button");
+
+selectTag.forEach((tag, id) => {
+   for (const country_code in countries) {
+    let selected;
+    if(id == 0 && country_code == "en-GB") {
+        selected = "selected";
+    } else if(id == 1 && country_code == "hi-IN") {
+        selected = "selected";
+    }
+      let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
+      tag.insertAdjacentHTML("beforeend", option);
+   }
+});
+
+translateBtn.addEventListener("click", () => {
+  let text = fromText.value,
+  translateFrom = selectTag[0].value,
+  translateTo = selectTag[1].value;
+   let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+   fetch(apiUrl).then(res => res.json()).then(data => {
+     console.log(data);
+     toText.value = data.responseData.translatedText;
+   });
+});
